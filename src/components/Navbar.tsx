4e6f4af83d1d462194b505544f3f9a1b62bea893
felipesousac <claudioflsousa@gmail.com>
2024-02-18 22:24:54 -0300

    Create static home page content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
-import { PlusCircle, Home } from "lucide-react";
+import { PlusCircle, Home, Component } from "lucide-react";
 
 export function Navbar() {
   return (
     <div className="w-full bg-[#436850] shadow-xl">
       <div className="mx-auto max-w-6xl text-[#FBFADA] p-4 flex items-center justify-between">
-        <div className="text-xl">Inventory</div>
+        <div className="text-xl flex gap-2 items-center">
+          <Component /> <span>Inventory</span>
+        </div>
         <div className="flex items-center gap-5">
           <button className="hover:bg-[#12372A] rounded-full p-2 transition-colors">
             <Home />
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,28 @@
+import { Plus, List } from "lucide-react";
+
 export function Home() {
   return (
     <div className="mx-auto max-w-6xl">
       <div className="text-[#12372A] text-2xl mx-auto flex flex-col items-center py-4 gap-3">
         <div>Home</div>
         <div className="mb-6 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-700 to-transparent opacity-25 dark:opacity-100 w-full" />
-        <div className="flex flex-col items-center justify-center gap-6">
-          <div>Mock 1</div>
-          <div>Mock 2</div>
+        <div className="flex flex-col items-center justify-center gap-6 w-full">
+          <div className="flex flex-col items-center justify-evenly border border-[#12372A] w-9/12 h-36">
+            <div>List of categories</div>
+            <List />
+          </div>
+          <div className="flex flex-col items-center justify-evenly border border-[#12372A] w-9/12 h-36">
+            <div>Create</div>
+            <Plus />
+          </div>
+          <div className="flex flex-col items-center justify-evenly border border-[#12372A] w-9/12 h-36">
+            <div>Total items</div>
+            <div>0</div>
+          </div>
+          <div className="flex flex-col items-center justify-evenly border border-[#12372A] w-9/12 h-36">
+            <div>Registered categories</div>
+            <div>0</div>
+          </div>
         </div>
       </div>
     </div>
