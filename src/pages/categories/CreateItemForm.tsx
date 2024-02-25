6d3045a9f440825766a6ea2476cc1da359e43563
felipesousac <claudioflsousa@gmail.com>
2024-02-25 19:08:00 -0300

    Add validation on form and POST request by submiting the form

--- src/pages/categories/CreateItemForm.tsx
+++ src/pages/categories/CreateItemForm.tsx
@@ -1,14 +1,61 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { LucideX, LucideCheck } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
+const createItemSchema = z.object({
+  itemName: z
+    .string()
+    .min(3, { message: "Minimum 3 characters" })
+    .max(30, { message: "Maximum 30 characters" }),
+  description: z.string().max(50, { message: "Maximum 50 characters" }),
+  categoryId: z.coerce.number(),
+  price: z.coerce.number().gt(0, { message: "Must be more than zero" }),
+  numberInStock: z.coerce
+    .number()
+    .nonnegative({ message: "Must not be less than zero" }),
+});
+
+type CreateItemSchema = z.infer<typeof createItemSchema>;
 
 export function CreateItemForm() {
+  const { register, handleSubmit } = useForm<CreateItemSchema>({
+    resolver: zodResolver(createItemSchema),
+  });
+
+  async function createItem({
+    itemName,
+    description,
+    categoryId,
+    price,
+    numberInStock,
+  }: CreateItemSchema) {
+    const headers = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    await axios
+      .post(
+        "http://localhost:8080/items",
+        { itemName, description, categoryId, price, numberInStock },
+        headers
+      )
+      .then((response) => {
+        console.log("It worked");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="w-full h-16 bg-[#436850] shadow-xl ">
-      <div className="mx-auto max-w-6xl flex items-center justify-between text-[#FBFADA]">
+      <div className="mx-auto max-w-6xl flex items-center text-[#FBFADA]">
         <Dialog.Title className="text-xl p-4">New Item</Dialog.Title>
-        <Dialog.Close className="p-2 mr-2 rounded-full hover:bg-[#12372A] transition-colors">
-          <LucideX className="size-5" />
-        </Dialog.Close>
       </div>
       <Dialog.Description className="mx-auto max-w-6xl p-4 text-[#12372A]/90 tracking-tight flex flex-col items-left justify-start gap-4">
         <p>
@@ -16,13 +63,15 @@ export function CreateItemForm() {
           below
         </p>
         <div className="mb-4 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-700 to-transparent opacity-25 dark:opacity-100 w-full" />
-        <form className="space-y-2">
+        <form className="space-y-2" onSubmit={handleSubmit(createItem)}>
           <div className="space-y-2">
             <label htmlFor="name" className="block font-medium">
               Name
             </label>
             <input
-              id="name"
+              {...register("itemName")}
+              placeholder="Name"
+              id="itemName"
               type="text"
               className="border border-[#12372A] rounded-lg px-3 py-2 w-full"
             />
@@ -33,6 +82,8 @@ export function CreateItemForm() {
               Description
             </label>
             <input
+              {...register("description")}
+              placeholder="Description"
               id="description"
               type="text"
               className="border border-[#12372A] rounded-lg px-3 py-2 w-full"
@@ -44,8 +95,23 @@ export function CreateItemForm() {
               Unit Price
             </label>
             <input
+              {...register("price")}
+              placeholder="Unit Price"
               id="unitPrice"
-              type="text"
+              type="number"
+              className="border border-[#12372A] rounded-lg px-3 py-2 w-full"
+            />
+          </div>
+
+          <div className="space-y-2">
+            <label htmlFor="numberInStock" className="block font-medium">
+              Number in Stock
+            </label>
+            <input
+              {...register("numberInStock")}
+              placeholder="Number in Stock"
+              id="numberInStock"
+              type="number"
               className="border border-[#12372A] rounded-lg px-3 py-2 w-full"
             />
           </div>
@@ -55,12 +121,12 @@ export function CreateItemForm() {
               Category
             </label>
             <select
-              name="categoryId"
               className="border border-[#12372A] rounded-lg px-3 py-2 w-full"
+              {...register("categoryId")}
             >
               <option selected>Select a category</option>
-              <option value="1">Capital Goods</option>
-              <option value="2">Beauty</option>
+              <option value={1}>Capital Goods</option>
+              <option value={2}>Beauty</option>
             </select>
           </div>
 
@@ -72,7 +138,10 @@ export function CreateItemForm() {
               </button>
             </Dialog.Close>
 
-            <button className="flex items-center tracking-tight gap-2 p-2 rounded-lg text-[#FBFADA] bg-[#436850]">
+            <button
+              type="submit"
+              className="flex items-center tracking-tight gap-2 p-2 rounded-lg text-[#FBFADA] bg-[#436850]"
+            >
               <LucideCheck className="size-4" />
               Register
             </button>
