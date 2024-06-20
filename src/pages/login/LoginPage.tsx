7d2a81d3094a2280a283c5b6b179c444f70bcbe7
felipesousac <claudioflsousa@gmail.com>
2024-06-19 22:50:13 -0300

    Add spin loader in login

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { SpinLoader } from "@/components/SpinLoader";
 import { useAuth } from "@/contexts/authProvider/useAuth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
@@ -12,6 +13,7 @@ const loginFields = z.object({
 type LoginFieldsSchema = z.infer<typeof loginFields>;
 
 export function LoginPage() {
+  const [isLogin, setIsLogin] = useState(false);
   const [isInvalid, setIsInvalid] = useState(false);
   const [credentialsExMessage, setCredentialsExMessage] = useState("");
 
@@ -23,8 +25,10 @@ export function LoginPage() {
 
   async function tryLogin({ username, userPass }: LoginFieldsSchema) {
     try {
+      setIsLogin(true);
       await authenticate(username, userPass);
     } catch (error: any) {
+      setIsLogin(false);
       checkExStatus(error);
     }
   }
@@ -40,51 +44,59 @@ export function LoginPage() {
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-5/6 max-w-md">
       <form
         onSubmit={handleSubmit(tryLogin)}
-        className="w-full rounded-md border border-[#436850]/60 flex flex-col items-center justify-center gap-4 py-4"
+        className="w-full rounded-md border border-[#436850]/60 "
       >
-        <div className="text-lg font-medium">Login</div>
-        <div className="flex flex-col gap-2 w-2/3">
-          <label htmlFor="username">Username</label>
-          <input
-            {...register("username")}
-            type="text"
-            id="username"
-            placeholder="Username"
-            className="px-1"
-          />
-          {formState.errors?.username?.message && (
-            <p className="text-sm text-red-500">
-              * {formState.errors.username.message}
-            </p>
-          )}
-        </div>
-        <div className="flex flex-col gap-2 w-2/3">
-          <label htmlFor="userPass">Password</label>
-          <input
-            {...register("userPass")}
-            type="password"
-            id="userPass"
-            placeholder="Password"
-            className="px-1"
-          />
-          {formState.errors?.userPass?.message && (
-            <p className="text-sm text-red-500">
-              * {formState.errors.userPass.message}
-            </p>
-          )}
-        </div>
-        {isInvalid && (
-          <div className="flex flex-col gap-2 w-2/3 text-red-500 text-sm">
-            {`* ${credentialsExMessage}`}
-          </div>
-        )}
-
-        <button
-          type="submit"
-          className="bg-[#436850] text-white px-3 py-1 rounded-md block w-2/3"
+        <fieldset
+          disabled={isLogin}
+          className="group flex flex-col items-center justify-center gap-4 py-4"
         >
-          Login
-        </button>
+          <div className="text-lg font-medium">Login</div>
+          <div className="flex flex-col gap-2 w-2/3">
+            <label htmlFor="username">Username</label>
+            <input
+              {...register("username")}
+              type="text"
+              id="username"
+              placeholder="Username"
+              className="px-1"
+            />
+            {formState.errors?.username?.message && (
+              <p className="text-sm text-red-500">
+                * {formState.errors.username.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-2 w-2/3">
+            <label htmlFor="userPass">Password</label>
+            <input
+              {...register("userPass")}
+              type="password"
+              id="userPass"
+              placeholder="Password"
+              className="px-1"
+            />
+            {formState.errors?.userPass?.message && (
+              <p className="text-sm text-red-500">
+                * {formState.errors.userPass.message}
+              </p>
+            )}
+          </div>
+          {isInvalid && (
+            <div className="flex flex-col gap-2 w-2/3 text-red-500 text-sm">
+              {`* ${credentialsExMessage}`}
+            </div>
+          )}
+
+          <button
+            type="submit"
+            className="group-disabled:pointer-events-none bg-[#436850] text-white px-3 py-1 rounded-md w-2/3 items-center justify-center flex"
+          >
+            <div className="absolute">
+              <SpinLoader className="h-4 w-4 group-enabled:opacity-0 border-t-[#FBFADA] border-l-[#FBFADA] border-b-[#FBFADA]" />
+            </div>
+            <div className="gap-2 group-disabled:opacity-0">Login</div>
+          </button>
+        </fieldset>
       </form>
     </div>
   );
